cc0e88a6d61132532aca55d2f6230643a3d58f75
Pantherstich <132139666+Pantherstich@users.noreply.github.com>
2023-08-31 15:32:39 +0300

    task-02 complete

---
+++ js/task-02.js
@@ -0,0 +1,34 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+
+const list = document.querySelector("#ingredients");
+
+ingredients.forEach(ingredient => {
+    const item = document.createElement("li");  
+    item.textContent = ingredient;
+    item.classList.add("item")
+ list.append(item);
+   
+})
+
+// // Adds an item to the end of the list
+// const lastItem = document.createElement("li");
+// lastItem.textContent = "Poly";
+// list.append(lastItem);
+
+// // Adds an item to the beginning of the list
+// const firstItem = document.createElement("li");
+// firstItem.textContent = "Ajax";
+// list.prepend(firstItem);
+
+// // Adds a title before the list
+// const title = document.createElement("h2");
+// title.textContent = "USERNAMES";
+// list.before(title);
