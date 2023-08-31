0bfb5e216f0596ec4dac22c7f50f99b42c0134d9
Pantherstich <132139666+Pantherstich@users.noreply.github.com>
2023-08-31 15:33:01 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -17,18 +17,3 @@ ingredients.forEach(ingredient => {
  list.append(item);
    
 })
-
-// // Adds an item to the end of the list
-// const lastItem = document.createElement("li");
-// lastItem.textContent = "Poly";
-// list.append(lastItem);
-
-// // Adds an item to the beginning of the list
-// const firstItem = document.createElement("li");
-// firstItem.textContent = "Ajax";
-// list.prepend(firstItem);
-
-// // Adds a title before the list
-// const title = document.createElement("h2");
-// title.textContent = "USERNAMES";
-// list.before(title);
