d0356d35a53551cf217d05efbdfc44c8f435bb78
Pantherstich <132139666+Pantherstich@users.noreply.github.com>
2023-08-31 14:51:47 +0300

    task-01 complete

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,14 @@
-const categories=document.querySelectorAll(".item");
+// ================1.1=============================
+
+const categories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categories.length}`);
+
+// ================1.2=============================
+
+categories.forEach(category => {
+  const items = category.querySelectorAll("li");
+  console.log(`Category: ${category.querySelector("h2").textContent}`);
+  console.log(`Elements: ${category.querySelectorAll("li").length}`);
+}
+)
+
