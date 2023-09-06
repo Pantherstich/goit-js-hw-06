38d9ba2ec435c5c717d39dde0524c834e42e2cd7
Pantherstich <pantherstich@gmail.com>
2023-09-06 23:46:43 +0300

    update task-02

--- js/task-02.js
+++ js/task-02.js
@@ -8,15 +8,16 @@ const ingredients = [
 ];
 const list = document.querySelector("#ingredients");
 
-// const items = [];
-// ingredients.forEach(ingredient => {
-//   const item = document.createElement("li");
-//   item.textContent = ingredient;
-//   item.classList.add("item");
-//   items.push(item);
-// })
-// list.append(...items)
+const items = [];
+ingredients.map(ingredient => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  items.push(item);
+})
+list.append(...items)
 
-const items = ingredients.map(ingredient => `<li class="item">${ingredient}</li>`)
-  .join('');
-list.insertAdjacentHTML('beforeend', items);
+
+// const items = ingredients.map(ingredient => `<li class="item">${ingredient}</li>`)
+//   .join('');
+// list.insertAdjacentHTML('beforeend', items);
